2a1336c77ba300ac2623897677e52005dfb80144
LaCrest Reed <lacrestreed@gmail.com>
2018-04-06 21:49:22 +0000

    Added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about'
+  get 'home/lookup'
+  post "/home/lookup" => 'home#lookup'
+  
+  root 'home#index' #short cut for above
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
